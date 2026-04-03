6ee736e8a5ae51cd6b68511c75f83b805793a579
hermes <hermes@rvlabs>
2026-04-03 05:17:33 +0100

    feat: wire onboarding flow into app startup

--- app/src/main/java/com/nomad/android/NomadApp.kt
+++ app/src/main/java/com/nomad/android/NomadApp.kt
@@ -5,10 +5,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.nomad.android.ui.navigation.NomadNavHost
+import com.nomad.android.ui.onboarding.OnboardingScreen
 import com.nomad.android.ui.theme.CrtScreen
 import com.nomad.android.ui.theme.NomadTheme
 import com.nomad.android.ui.theme.PipBoyBottomNav
@@ -20,22 +24,28 @@ class NomadApp : Application()
 
 @Composable
 fun NomadApp() {
-    val navController = rememberNavController()
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentRoute = navBackStackEntry?.destination?.route ?: "dashboard"
+    var onboardingComplete by rememberSaveable { mutableStateOf(false) }
 
-    NomadTheme {
-        CrtScreen {
-            Column(modifier = Modifier.fillMaxSize()) {
-                PipBoyStatusBar()
-                NomadNavHost(
-                    navController = navController,
-                    modifier = Modifier.weight(1f),
+    if (!onboardingComplete) {
+        NomadTheme {
+            CrtScreen {
+                OnboardingScreen(
+                    onComplete = { onboardingComplete = true }
                 )
-                PipBoyBottomNav(
-                    navController = navController,
-                    currentRoute = currentRoute,
-                )
+            }
+        }
+    } else {
+        val navController = rememberNavController()
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route ?: "dashboard"
+
+        NomadTheme {
+            CrtScreen {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    PipBoyStatusBar()
+                    NomadNavHost(navController = navController, modifier = Modifier.weight(1f))
+                    PipBoyBottomNav(navController = navController, currentRoute = currentRoute)
+                }
             }
         }
     }
