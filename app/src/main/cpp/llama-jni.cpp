fd6bbca530e44c6e07419d26df6d0e6c3c8f0a15
Raul <raul.vidis@gmail.com>
2026-05-30 00:49:40 +0300

    chore: rename JNI logcat tag opendroid-llm -> nomad-llm

--- app/src/main/cpp/llama-jni.cpp
+++ app/src/main/cpp/llama-jni.cpp
@@ -18,7 +18,7 @@
 #include "chat.h"
 #include "nlohmann/json.hpp"
 
-#define LOG_TAG    "opendroid-llm"
+#define LOG_TAG    "nomad-llm"
 #define LOGI(...)  __android_log_print(ANDROID_LOG_INFO,  LOG_TAG, __VA_ARGS__)
 #define LOGW(...)  __android_log_print(ANDROID_LOG_WARN,  LOG_TAG, __VA_ARGS__)
 #define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
